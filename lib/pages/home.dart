45b2c08dda560072269b1861fef4ec0baadbe80f
felipe.ardila <felipe.ardila@valid.com>
2022-06-10 10:35:47 -0500

    App Terminada

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 import 'package:band_name/models/band.dart';
+import 'package:band_name/services/socket_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,16 +14,46 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Band> bands = [
-    Band(id: '1', name: 'Metalica', votes: 5),
-    Band(id: '1', name: 'queen', votes: 1),
-    Band(id: '1', name: 'Heroes del silencio', votes: 2),
-    Band(id: '1', name: 'Bon javi', votes: 5),
+    // Band(id: '1', name: 'Metalica', votes: 5),
+    // Band(id: '1', name: 'queen', votes: 1),
+    // Band(id: '1', name: 'Heroes del silencio', votes: 2),
+    // Band(id: '1', name: 'Bon javi', votes: 5),
   ];
+
+  @override
+  void initState() {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    socketService.socket.on('active-bands', _handleActiveBands);
+    super.initState();
+  }
+
+  _handleActiveBands(dynamic payload) {
+    bands = (payload as List).map((band) => Band.fromMap(band)).toList();
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    socketService.socket.off('active-bands');
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final socketService = Provider.of<SocketService>(context);
+
     return Scaffold(
       appBar: AppBar(
         elevation: 1,
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: (socketService.serverStatus == ServerStatus.online)
+                ? Icon(Icons.check_circle, color: Colors.blue[300])
+                : Icon(Icons.offline_bolt, color: Colors.red),
+          )
+        ],
         title: const Center(
             child: Text(
           'BandNames',
@@ -28,9 +61,16 @@ class _HomePageState extends State<HomePage> {
         )),
         backgroundColor: Colors.white,
       ),
-      body: ListView.builder(
-        itemCount: bands.length,
-        itemBuilder: (context, i) => _bandTile(bands[i]),
+      body: Column(
+        children: [
+          _showGrapg(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: bands.length,
+              itemBuilder: (context, i) => _bandTile(bands[i]),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: addNewBand,
@@ -41,14 +81,11 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _bandTile(Band band) {
+    final socketService = Provider.of<SocketService>(context, listen: false);
     return Dismissible(
       key: Key(band.id),
       direction: DismissDirection.startToEnd,
-      onDismissed: (direction) {
-        print('direction: $direction');
-        print('id: ${band.id}');
-        //TODO llamar el borrado en el server
-      },
+      onDismissed: (_) => socketService.emit('remove-band', {'id': band.id}),
       background: Container(
         padding: EdgeInsets.only(left: 8.0),
         color: Colors.red,
@@ -67,9 +104,7 @@ class _HomePageState extends State<HomePage> {
         ),
         title: Text(band.name),
         trailing: Text('${band.votes}', style: const TextStyle(fontSize: 20)),
-        onTap: () {
-          print('${band.name}');
-        },
+        onTap: () => socketService.socket.emit('vote-band', {'id': band.id}),
       ),
     );
   }
@@ -79,56 +114,71 @@ class _HomePageState extends State<HomePage> {
     if (Platform.isAndroid) {
       return showDialog(
         context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('New band name'),
-            content: TextField(
-              controller: textController,
-            ),
-            actions: [
-              MaterialButton(
-                  child: Text('Add'),
-                  textColor: Colors.blue,
-                  elevation: 5,
-                  onPressed: () => addBandToList(textController.text))
-            ],
-          );
-        },
+        builder: (_) => AlertDialog(
+          title: Text('New band name'),
+          content: TextField(
+            controller: textController,
+          ),
+          actions: [
+            MaterialButton(
+                child: Text('Add'),
+                textColor: Colors.blue,
+                elevation: 5,
+                onPressed: () => addBandToList(textController.text))
+          ],
+        ),
       );
     }
 
     showCupertinoDialog(
       context: context,
-      builder: (_) {
-        return CupertinoAlertDialog(
-          title: Text('new band name'),
-          content: CupertinoTextField(
-            controller: textController,
+      builder: (_) => CupertinoAlertDialog(
+        title: Text('new band name'),
+        content: CupertinoTextField(
+          controller: textController,
+        ),
+        actions: [
+          CupertinoDialogAction(
+            child: Text('Add'),
+            isDefaultAction: true,
+            onPressed: () => addBandToList(textController.text),
           ),
-          actions: [
-            CupertinoDialogAction(
-              child: Text('Add'),
-              isDefaultAction: true,
-              onPressed: () => addBandToList(textController.text),
-            ),
-            CupertinoDialogAction(
-                isDestructiveAction: true,
-                child: Text('Dismiss'),
-                onPressed: () => Navigator.pop(context)),
-          ],
-        );
-      },
+          CupertinoDialogAction(
+              isDestructiveAction: true,
+              child: Text('Dismiss'),
+              onPressed: () => Navigator.pop(context)),
+        ],
+      ),
     );
   }
 
   void addBandToList(String name) {
-    print(name);
     if (name.length > 1) {
-      //agregar
-      bands.add(Band(id: DateTime.now().toString(), name: name, votes: 0));
-      setState(() {});
+      // agregar
+      final socketService = Provider.of<SocketService>(context, listen: false);
+      socketService.socket.emit('add-band', {'name': name});
     }
 
     Navigator.pop(context);
+  }
+
+  Widget _showGrapg() {
+    Map<String, double> dataMap = {};
+
+    bands.forEach((band) {
+      dataMap.putIfAbsent(band.name, () => band.votes.toDouble());
+    });
+
+    return Container(
+        padding: EdgeInsets.all(15),
+        child: PieChart(
+          dataMap: dataMap,
+          animationDuration: Duration(milliseconds: 800),
+          chartValuesOptions: const ChartValuesOptions(
+              showChartValueBackground: false,
+              showChartValuesInPercentage: true,
+              showChartValuesOutside: false),
+          chartType: ChartType.ring,
+        ));
   }
 }
---
+++ lib/services/socket_service.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus {
+  online,
+  offline,
+  connecting,
+}
+
+class SocketService with ChangeNotifier {
+  late ServerStatus _serverStatus = ServerStatus.connecting;
+  late IO.Socket _socket;
+
+  ServerStatus get serverStatus => _serverStatus;
+
+  IO.Socket get socket => _socket;
+  Function get emit => _socket.emit;
+
+  SocketService() {
+    _initConfig();
+  }
+
+  void _initConfig() {
+    // Dart client
+    _socket = IO.io(
+        'http://192.168.1.31:3000/',
+        IO.OptionBuilder()
+            .setTransports(['websocket'])
+            .enableAutoConnect()
+            .build());
+
+    _socket.onConnect((_) {
+      _serverStatus = ServerStatus.online;
+      notifyListeners();
+    });
+
+    _socket.onDisconnect((_) {
+      _serverStatus = ServerStatus.offline;
+      notifyListeners();
+    });
+  }
+}
